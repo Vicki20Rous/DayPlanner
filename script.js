4b7961e419817c01104ae4f4b71e696b00dcd185
Victoria <vic86rous@gmail.com>
2021-04-25 16:00:26 -0500

    updated script.js file

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
 $(document).ready(function(){
 
-    $("#currentDay").text(moment().format("MMM Do YY"));
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
     $(".saveBtn").on("click", function() {
-        
+
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time. text);
     })
+
+    $("#hour7 .description").val(localStorage.getItem("hour7"));
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    
+    function timeKeeper() {
+        
+        var currentHour = moment().hour();
+
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockHour, currentHour) 
+
+            if(blockHour < currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else{
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future"); 
+            }
+        
+        })
+
+    }
+    timeKeeper();
 })
